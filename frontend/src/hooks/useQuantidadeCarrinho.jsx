f6151a2fbbf65bc506fa94deec81fa05b80940ad
Mauricio Pereira <mauriciobraga100@gmail.com>
2023-08-14 09:09:25 -0300

    .

--- frontend/src/hooks/useQuantidadeCarrinho.jsx
+++ frontend/src/hooks/useQuantidadeCarrinho.jsx
@@ -4,14 +4,21 @@ const useQuantidadeCarrinho = () => {
   const [quantidadeCarrinho, setQuantidadeCarrinho] = useState(0);
 
   useEffect(() => {
-    fetch('http://localhost:3000/carrinho/quantidade')
-      .then(response => response.json())
-      .then(data => {
+    const fetchQuantidade = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/carrinho/quantidade');
+        const data = await response.json();
         setQuantidadeCarrinho(data.quantidade);
-      })
-      .catch(error => {
+      } catch (error) {
         console.error("Erro ao buscar a quantidade:", error);
-      });
+      }
+    };
+
+    fetchQuantidade();
+
+    const interval = setInterval(fetchQuantidade, 300);
+
+    return () => clearInterval(interval);
   }, []);
 
   return quantidadeCarrinho;
